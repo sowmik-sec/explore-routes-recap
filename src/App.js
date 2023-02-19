c35d56df78a9d90a257f6e226c03d23fc646504a
sowmik-sec <sowmik.sec@gmail.com>
2023-02-19 16:04:11 +0600

    header, home, about and products components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <div>Default Page</div> },
-    { path: "/home", element: <div>Home Page</div> },
-    { path: "/about", element: <div>About Page</div> },
+    { path: "/home", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/products", element: <Products /> },
   ]);
   return (
     <div className="App">
---
+++ src/components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const Products = () => {
+  return (
+    <div>
+      <Header></Header>
+      These are products.
+    </div>
+  );
+};
+
+export default Products;
